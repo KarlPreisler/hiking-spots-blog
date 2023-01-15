2bb198205223f9d9ddc4f2cb92d2ce5aa85acf6a
KarlPreisler <Karlpreisler@gmail.com>
2023-01-15 16:15:49 +0000

    Fix allowed host error

--- hikingspots/settings.py
+++ hikingspots/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['hikingspots.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['hiking-spots.herokuapp.com', 'localhost']
 
 
 # Application definition
