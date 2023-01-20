92cdbf5492a3bf1a97c13825d65f414b16e338ae
KarlPreisler <Karlpreisler@gmail.com>
2023-01-20 15:45:40 +0000

    change url path for edit post

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('add_post', views.AddPost.as_view(), name='add_post'),
-    path('<slug:slug>/edit/<int:pk>', views.EditPost.as_view(),
+    path('edit/<int:pk>', views.EditPost.as_view(),
          name='edit_post'),
 ]
