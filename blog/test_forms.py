98aed3cdd32c84f7c5c810a4b5bcc7b3b0699a68
KarlPreisler <Karlpreisler@gmail.com>
2023-01-24 16:44:10 +0000

    Add passing automated tests for forms.py

---
+++ blog/test_forms.py
@@ -0,0 +1,78 @@
+from django.test import TestCase
+from .forms import CommentForm, AddPostForm, EditPostForm
+
+
+class TestCommentForm(TestCase):
+    def test_message_field_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('body', form.errors.keys())
+        self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = CommentForm()
+        self.assertEqual(form.Meta.fields, ('body',))
+
+
+class TestAddPostForm(TestCase):
+
+    def test_title_is_required(self):
+        form = AddPostForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_slug_is_required(self):
+        form = AddPostForm({'slug': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('slug', form.errors.keys())
+        self.assertEqual(form.errors['slug'][0], 'This field is required.')
+
+    def test_content_is_required(self):
+        form = AddPostForm({'content': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors.keys())
+        self.assertEqual(form.errors['content'][0], 'This field is required.')
+
+    def test_difficulty_is_required(self):
+        form = AddPostForm({'difficulty': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('difficulty', form.errors.keys())
+        self.assertEqual(
+            form.errors['difficulty'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = AddPostForm()
+        self.assertEqual(form.Meta.fields, ('__all__'))
+
+
+class TestEditPostForm(TestCase):
+
+    def test_title_is_required(self):
+        form = EditPostForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors.keys())
+        self.assertEqual(form.errors['title'][0], 'This field is required.')
+
+    def test_slug_is_required(self):
+        form = EditPostForm({'slug': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('slug', form.errors.keys())
+        self.assertEqual(form.errors['slug'][0], 'This field is required.')
+
+    def test_content_is_required(self):
+        form = EditPostForm({'content': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors.keys())
+        self.assertEqual(form.errors['content'][0], 'This field is required.')
+
+    def test_difficulty_is_required(self):
+        form = EditPostForm({'difficulty': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('difficulty', form.errors.keys())
+        self.assertEqual(
+            form.errors['difficulty'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = EditPostForm()
+        self.assertEqual(form.Meta.fields, ('__all__'))
---
+++ blog/test_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+# class TestViews(TestCase):
+#
+#    def test_get_post_list_view(self):
+#        response = self.client.get('/')
+#        self.assertEqual(response.status_code, 200)
+#        self.assertTemplateUsed(response, 'blog/base.html')
+#        self.assertTemplateUsed(response, 'index.html')
