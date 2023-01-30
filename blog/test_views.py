463596026c556b6f83c18c2a231f53c8c10e465c
KarlPreisler <Karlpreisler@gmail.com>
2023-01-30 20:02:41 +0000

    Add about us content, fix typing errors

--- blog/test_views.py
+++ blog/test_views.py
@@ -6,4 +6,5 @@ class TestViews(TestCase):
     def test_get_post_list_view(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'index.html')
+        self.assertTemplateUsed(response, 'blog/index.html')
+        self.assertTemplateUsed(response, 'base.html')
