11e4de3e4734f16d761be2f01d161341858640b1
KarlPreisler <Karlpreisler@gmail.com>
2023-01-31 16:01:30 +0000

    Fix image path

--- hikingspots/settings.py
+++ hikingspots/settings.py
@@ -33,9 +33,28 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = 'DEV' in os.environ
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['hiking-spots.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ["hiking-spots.herokuapp.com", "localhost"]
 
 
 # Application definition
